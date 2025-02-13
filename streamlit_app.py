fde8a26c0a49aa6129bc886376ebaea92007758d
Fran Vidal <95777316+Haplee@users.noreply.github.com>
2025-02-13 12:44:16 +0000

    Primera version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 Mi app")
+st.write("Vamos a empezar con estoo.")
+
+st.write("Practicando mi nueva app")
+
+st.header("Esto es una cabezera")
+
+cantidad=st.slider("Elija un valor")
+
+print(f' el identificador de cantidad es {id(cantidad)}')
+
+
+for i in range(cantidad):
+    st.button(f'Botón{i}')
+    st.checkbox (f'Opcion{i}')
